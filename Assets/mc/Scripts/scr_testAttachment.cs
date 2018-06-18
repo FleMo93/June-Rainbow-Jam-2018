9e29c4309fa0b6910521a5618d2f317de9da17e1
mcvenor <michal.j.cu@gmail.com>
2018-06-18 19:59:19 +0200

    added: - Idle Animation for Human - item_axe - item_bag - test with attachment points

---
+++ Assets/mc/Scripts/scr_testAttachment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_testAttachment : MonoBehaviour {
+
+    public Transform AttachingObject;
+    public Transform AttachmentPoint;
+
+	// Use this for initialization
+	void Start () {
+        if (AttachingObject != null && AttachmentPoint != null)
+        {
+            AttachingObject.parent = AttachmentPoint;
+            AttachingObject.localPosition = Vector3.zero;
+            AttachingObject.localRotation = Quaternion.identity;
+            AttachingObject.localScale = Vector3.one;
+        }
+	}
+	
+}
