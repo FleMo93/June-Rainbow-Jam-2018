0c44885003ba102bdecf8e78839493b9dc122b46
Florian Hammer <florianhammer@hotmail.de>
2018-06-24 21:49:52 +0200

    Start to add tree animation and models when felled

--- Assets/Flo/Code/Objects/scr_Tree.cs
+++ Assets/Flo/Code/Objects/scr_Tree.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 
 public class scr_Tree : MonoBehaviour, i_Interactable, i_Damageable {
     scr_Stats stats;
+    [SerializeField]
+    private ParticleSystem _ParticleOnDamage;
+    [SerializeField]
+    private GameObject _MainModel;
+    [SerializeField]
+    private GameObject _Stub;
+    [SerializeField]
+    private GameObject _FelledBody;
 
     void Start () {
         stats = gameObject.GetComponent<scr_Stats>();
+        _Stub.SetActive(false);
+        _FelledBody.SetActive(false);
 	}
 
-    public void Damage(int damage)
+    public void Damage(int damage) 
     {
         stats.Health -= damage;
+        _ParticleOnDamage.Play();
+
+        if(stats.Health <= 0)
+        {
+            _Stub.SetActive(true);
+            _FelledBody.SetActive(true);
+            _MainModel.SetActive(false);
+        }
     }
 
     public scr_Interactable_Result Interact(GameObject trigger, scr_Stats.ObjectType itemInInventory)
